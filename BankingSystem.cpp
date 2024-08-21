849be44d89f79f87c374875d409eb120a7954ab0
AshithKumarGowda <166153550+isAshithKumarGowda@users.noreply.github.com>
2024-08-21 19:40:17 +0530

    Update BankingSystem.cpp

--- BankingSystem.cpp
+++ BankingSystem.cpp
@@ -1,336 +1,269 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <stdexcept>
+
 using namespace std;
-class shopping{
-    private:
-        int pcode;
-        float price;
-        float dis;
-        string pname;
 
-    public:
-        void menu();
-        void administrator();
-        void buyer();
-        void edit();
-        void add();
-        void rem();
-        void list();
-        void receipt();
+class InsufficientFunds {};
 
+class Account {
+private:
+    long accountNumber;
+    string firstName;
+    string lastName;
+    float balance;
+    static long NextAccountNumber;
+public:
+    Account() {}
+    Account(string fname, string lname, float balance);
+    long getAccNo() const { return accountNumber; }
+    string getFirstName() const { return firstName; }
+    string getLastName() const { return lastName; }
+    float getBalance() const { return balance; }
+
+    void Deposit(float amount);
+    void Withdraw(float amount);
+    static void setLastAccountNumber(long accountNumber);
+    static long getLastAccountNumber();
+    friend ofstream& operator<<(ofstream& ofs, const Account& acc);
+    friend ifstream& operator>>(ifstream& ifs, Account& acc);
+    friend ostream& operator<<(ostream& os, const Account& acc);
 };
-void shopping :: menu(){
-    m:
+
+long Account::NextAccountNumber = 0;
+
+class Bank {
+private:
+    map<long, Account> accounts;
+public:
+    Bank();
+    Account OpenAccount(string fname, string lname, float balance);
+    Account BalanceEnquiry(long accountNumber) const;
+    Account Deposit(long accountNumber, float amount);
+    Account Withdraw(long accountNumber, float amount);
+    void CloseAccount(long accountNumber);
+    void ShowAllAccounts() const;
+    ~Bank();
+};
+
+int main() {
+    Bank b;
+    Account acc;
+
     int choice;
-    string email;
-    string password;
-
-    cout<<"\t\t\t\t__________________________________\n";
-    cout<<"\t\t\t\t                                  \n";
-    cout<<"\t\t\t\t       Supermarket Main Menu      \n";
-    cout<<"\t\t\t\t                                  \n";
-    cout<<"\t\t\t\t__________________________________\n";
-
-    cout<<"\t\t\t\t|   1) Administrator   |\n";
-    cout<<"\t\t\t\t|                      |\n";
-    cout<<"\t\t\t\t|   2) Buyer           |\n";
-    cout<<"\t\t\t\t|                      |\n";
-    cout<<"\t\t\t\t|   3) Exit            |\n";
-    cout<<"\t\t\t\tPlease select: ";
-    cin >>choice;
-    cout<<endl;
-
-    switch (choice){
+    string fname, lname;
+    long accountNumber;
+    float balance;
+    float amount;
+    cout << "***Banking System***" << endl;
+    do {
+        cout << "\n\tSelect one option below";
+        cout << "\n\t1 Open an Account";
+        cout << "\n\t2 Balance Enquiry";
+        cout << "\n\t3 Deposit";
+        cout << "\n\t4 Withdrawal";
+        cout << "\n\t5 Close an Account";
+        cout << "\n\t6 Show All Accounts";
+        cout << "\n\t7 Quit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        switch (choice) {
         case 1:
-            cout<<"\t\t\t\tPlease Login \n";
-            cout<<"\t\t\t\tEnter email \n";
-            cin>>email;
-            cout<<"\t\t\t\tPassword \n";
-            cin>>password;
-
-            if(email=="ashithkumargowda2005@gmail.com"&&password=="Hello"){
-                administrator();
-            }
-            else{
-                cout<<"Invalid email/password\n";
-            }
-            break;
-
-        case 2:
-            buyer();
-            break;
-        case 3:
-            exit (0);
-        default:
-            {
-                cout<<"Please select from the giben options";
-            }
-    }
-    goto m;
-}
-void shopping :: administrator(){
-    m:
-    int choice;
-    cout<<"\n\n\n\n\n\nAdministrator menu";
-    cout<<"\n\n\t\t\t\t|______1) Add the product______|";
-    cout<<"\n\n\t\t\t\t|                              |";
-    cout<<"\n\n\t\t\t\t|______2) Modify the product___|";
-    cout<<"\n\n\t\t\t\t|                              |";
-    cout<<"\n\n\t\t\t\t|______3) Delete the product___|";
-    cout<<"\n\n\t\t\t\t|                              |";
-    cout<<"\n\n\t\t\t\t|______4) Back to main menu____|\n";
-
-    cout<<"\t\t\t\tPlease enter your choice: ";
-    cin>>choice;
-    cout<<endl;
-    switch(choice){
-        case 1:
-            add();
+            cout << "Enter First Name: ";
+            cin >> fname;
+            cout << "Enter Last Name: ";
+            cin >> lname;
+            cout << "Enter initial Balance: ";
+            cin >> balance;
+            acc = b.OpenAccount(fname, lname, balance);
+            cout << endl << "Congratulations! Account is Created" << endl;
+            cout << acc;
             break;
         case 2:
-            edit();
+            cout << "Enter Account Number: ";
+            cin >> accountNumber;
+            try {
+                acc = b.BalanceEnquiry(accountNumber);
+                cout << endl << "Your Account Details" << endl;
+                cout << acc;
+            } catch (const runtime_error& e) {
+                cout << e.what() << endl;
+            }
             break;
         case 3:
-            rem();
+            cout << "Enter Account Number: ";
+            cin >> accountNumber;
+            cout << "Enter Amount: ";
+            cin >> amount;
+            try {
+                acc = b.Deposit(accountNumber, amount);
+                cout << endl << "Amount is Deposited" << endl;
+                cout << acc;
+            } catch (const runtime_error& e) {
+                cout << e.what() << endl;
+            }
             break;
         case 4:
-            menu();
+            cout << "Enter Account Number: ";
+            cin >> accountNumber;
+            cout << "Enter Amount: ";
+            cin >> amount;
+            try {
+                acc = b.Withdraw(accountNumber, amount);
+                cout << endl << "Amount Withdrawn" << endl;
+                cout << acc;
+            } catch (const runtime_error& e) {
+                cout << e.what() << endl;
+            } catch (const InsufficientFunds&) {
+                cout << "Insufficient funds" << endl;
+            }
+            break;
+        case 5:
+            cout << "Enter Account Number: ";
+            cin >> accountNumber;
+            b.CloseAccount(accountNumber);
+            cout << endl << "Account is Closed" << endl;
+            break;
+        case 6:
+            b.ShowAllAccounts();
+            break;
+        case 7:
             break;
         default:
-            cout<<"Invalid choice!";
-    }
-goto m;
-}
-void shopping::buyer(){
-    m:
-    int choice;
-    cout<<"\t\t\t\t Buyer \n";
-    cout<<"                  \n";
-    cout<<"\t\t\t\t1) Buy product\n";
-    cout<<"                    \n";
-    cout<<"\t\t\t\t2) Go back \n";
-    cout<<"\t\t\t\t Enter your choice : ";
-
-    cin>>choice;
-
-    switch (choice){
-        case 1:
-            receipt();
-            exit (0);
-        case 2:
-            menu();
-            break;
-        default :
-            cout<<"Invalid choice";
-    }
-    goto m;
-}
-void shopping ::add(){
-
-    m:
-    fstream data;
-    int c;
-    float p;
-    float d;
-    string n;
-
-    cout<<"\n\n\t\t\t\t Add new product";
-    cout<<"\n\n\t\tProduct code of the product: ";
-    cin>>pcode;
-    cout<<"\n\n\t\tName of the product: ";
-    cin>>pname;
-    cout<<"\n\n\t\tPrice of the product: ";
-    cin>>price;
-    cout<<"\n\n\t\tDiscount on product: ";
-    cin>>dis;
-
-    data.open("database.txt",ios::in);
-
-    if(!data){
-        data.open("database.txt",ios::app|ios::out);
-        data<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<"\n";
-        data.close();
-    }
-    else{
-        data>>c>>n>>p>>d;
-        int token=0;
-
-        while(!data.eof()){
-            if(c==pcode){
-                token++;
-            }
-            data>>c>>n>>p>>d;
+            cout << "\nEnter correct choice";
+            exit(0);
         }
-        data.close();
-        if(token==1){
-            cout<<"Product code exists!";
-            goto m;
-        }
-        else{
-            data.open("database.txt",ios::app|ios::out);
-            data<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<"\n";
-            data.close();
-        }
-    }
-    cout<<"\n\n\t\tRecord inserted!";
+    } while (choice != 7);
+    return 0;
 }
-void shopping::edit() {
-    fstream data, data1;
-    int pkey;
-    int token = 0;
-    int c;
-    float p;
-    float d;
-    string n;
 
-    cout << "\n\n\t\tModify the record";
-    cout << "\n\n\t\tProduct code :";
-    cin >> pkey;
-    
-    data.open("database.txt", ios::in);
-    if (!data) {
-        cout << "\n\nFile doesn't exist!";
-    } 
-    else {
-        data1.open("database1.txt", ios::app);
-        
-        data >> pcode >> pname >> price >> dis;
-        while (!data.eof()) {
-            if (pkey == pcode) {
-                cout << "\n\n\t\tProduct new code :";
-                cin >> c;
-                cout << "\n\n\t\tName of the product :";
-                cin >> n;
-                cout << "\n\n\t\tPrice :";
-                cin >> p;
-                cout << "\n\n\t\tDiscount :";
-                cin >> d;
-                data1 << " " << c << " " << n << " " << p << " " << d << "\n";
-                cout << "\n\n\t\tRecord edited";
-                token++;
-            } 
-            else {
-                data1 << " " << pcode << " " << pname << " " << price << " " << dis << "\n";
-            }
-            data >> pcode >> pname >> price >> dis; // Read the next record
-        }
-        data.close();
-        data1.close();
-        remove("database.txt");
-        rename("database1.txt", "database.txt");
+Account::Account(string fname, string lname, float balance) {
+    NextAccountNumber++;
+    accountNumber = NextAccountNumber;
+    firstName = fname;
+    lastName = lname;
+    this->balance = balance;
+}
 
-        if (token == 0) {
-            cout << "\n\nRecord not found!";
-        }
+void Account::Deposit(float amount) {
+    balance += amount;
+}
+
+void Account::Withdraw(float amount) {
+    if (balance - amount < 500) 
+        throw InsufficientFunds();
+    balance -= amount;
+}
+
+void Account::setLastAccountNumber(long accountNumber) {
+    NextAccountNumber = accountNumber;
+}
+
+long Account::getLastAccountNumber() {
+    return NextAccountNumber;
+}
+
+ofstream& operator<<(ofstream& ofs, const Account& acc) {
+    ofs << acc.accountNumber << endl;
+    ofs << acc.firstName << endl;
+    ofs << acc.lastName << endl;
+    ofs << acc.balance << endl;
+    return ofs;
+}
+
+ifstream& operator>>(ifstream& ifs, Account& acc) {
+    ifs >> acc.accountNumber;
+    ifs >> acc.firstName;
+    ifs >> acc.lastName;
+    ifs >> acc.balance;
+    return ifs;
+}
+
+ostream& operator<<(ostream& os, const Account& acc) {
+    os << "First Name: " << acc.getFirstName() << endl;
+    os << "Last Name: " << acc.getLastName() << endl;
+    os << "Account Number: " << acc.getAccNo() << endl;
+    os << "Balance: " << acc.getBalance() << endl;
+    return os;
+}
+
+Bank::Bank() {
+    Account account;
+    ifstream infile("Bank.data");
+    if (!infile) {
+        cout << "Error in Opening! File Not Found!!" << endl;
+        return;
+    }
+    while (infile >> account) {
+        accounts.insert({account.getAccNo(), account});
+    }
+    Account::setLastAccountNumber(account.getAccNo());
+}
+
+Account Bank::OpenAccount(string fname, string lname, float balance) {
+    ofstream outfile("Bank.data", ios::trunc);
+    if (!outfile) {
+        cerr << "Error opening file for writing!" << endl;
+        return Account();
+    }
+    Account account(fname, lname, balance);
+    accounts.insert({account.getAccNo(), account});
+    for (const auto& pair : accounts) {
+        outfile << pair.second;
+    }
+    return account;
+}
+
+Account Bank::BalanceEnquiry(long accountNumber) const {
+    auto itr = accounts.find(accountNumber);
+    if (itr == accounts.end()) {
+        throw runtime_error("Account not found.");
+    }
+    return itr->second;
+}
+
+Account Bank::Deposit(long accountNumber, float amount) {
+    auto itr = accounts.find(accountNumber);
+    if (itr == accounts.end()) {
+        throw runtime_error("Account not found.");
+    }
+    itr->second.Deposit(amount);
+    return itr->second;
+}
+
+Account Bank::Withdraw(long accountNumber, float amount) {
+    auto itr = accounts.find(accountNumber);
+    if (itr == accounts.end()) {
+        throw runtime_error("Account not found.");
+    }
+    itr->second.Withdraw(amount);
+    return itr->second;
+}
+
+void Bank::CloseAccount(long accountNumber) {
+    auto itr = accounts.find(accountNumber);
+    if (itr != accounts.end()) {
+        cout << "Account Deleted" << endl << itr->second;
+        accounts.erase(accountNumber);
+    } else {
+        cout << "Account not found." << endl;
     }
 }
-void shopping ::rem(){
-    fstream data,data1;
-    int pkey;
-    int token=0;
-    cout<<"\n\n\t\t\t\tDelete product";
-    cout<<"\n\n\t\t\t\tProduct code:";
-    cin>>pkey;
-    data.open("database.txt",ios::in);
-    if(!data){
-        cout<<"File dosent exist";
-    }
-    else{
-        data1.open("database1.txt",ios::app|ios::out);
-        data>>pcode>>pname>>price>>dis;
-        while(!data.eof()){
-            if(pcode==pkey){
-                cout<<"\n\n\t\t\t\tProduct deleted successfully";
-                token++;
-            }
-            else{
-                data1<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<"\n";
-            }
-            data>>pcode>>pname>>price>>dis;
-        }
-        data.close();
-        data1.close();
-        remove("database.txt");
-        rename("database1.txt","database.txt");
 
-        if(token==0){
-            cout<<"\n\n\t\t\t\tRecord not found";
-        }
+void Bank::ShowAllAccounts() const {
+    for (const auto& pair : accounts) {
+        cout << "Account " << pair.first << endl << pair.second << endl;
     }
 }
-void shopping::list(){
-    fstream data;
-    data.open("database.txt",ios::in);
-    cout<<"\n\n\t\t\t\t___________________________________________\n";
-    cout<<"\t\t\t\tProNo\t\tName\t\tPrice\n";
-    data>>pcode>>pname>>price>>dis;
-    while(!data.eof()){
-        cout<<"\t\t\t\t"<<pcode<<"\t\t"<<pname<<"\t\t"<<price<<"\n";
-        data>>pcode>>pname>>price>>dis;
+
+Bank::~Bank() {
+    ofstream outfile("Bank.data", ios::trunc);
+    if (!outfile) {
+        cerr << "Error opening file for writing!" << endl;
+        return;
     }
-
-    data.close();
-}
-void shopping::receipt(){
-    fstream data;
-
-    int arrc[100];
-    int arrq[100];
-    char choice;
-    int c=0;
-    float amount=0;
-    float dia=0;
-    float total=0;
-
-    data.open("database.txt",ios::in);
-    if(!data){
-        cout<<"\n\nEmpty database";
+    for (const auto& pair : accounts) {
+        outfile << pair.second;
     }
-    else{
-        data.close();
-
-        list();
-        cout<<"\n\t\t\t\t_________________________________________\n";
-        cout<<"\n                                         \n";
-        cout<<"\n\t\t\t\t          Please place the order         \n";
-        cout<<"\n                                         \n";
-        cout<<"\n\t\t\t\t_________________________________________\n";
-        do{
-            m:
-            cout<<"\n\n\t\t\t\tEnter Product code: ";
-            cin>>arrc[c];
-            cout<<"\n\n\t\t\t\tEnter the product quantity: ";
-            cin>>arrq[c];
-            c++;
-            cout<<"\n\n\t\t\t\tDo you want to buy another product? if yes press y else n : ";
-            cin>>choice;
-        }while(choice=='y');
-
-        cout<<"\n\n\t\t___________________________________________RECEPT_______________________________________________\n";
-        cout<<"\n\t\tProduct code\t Product Name\t Product quantity\t Price\t Amount\t Amount after Discount\n";
-        for(int i=0;i<c;i++){
-        data.open("database.txt",ios::in);
-        int proDis;
-        // for(int i=0;i<c;i++){
-            data>>pcode>>pname>>price>>dis;
-            while(!data.eof()){
-                if(pcode==arrc[i]){
-                    amount=price*arrq[i];
-                    proDis=amount-(amount*dis/100);
-                    total=total+proDis;
-                    cout<<"\n\t\t"<<pcode<<"\t\t "<<pname<<"\t "<<arrq[i]<<"\t\t\t "<<price<<"\t "<<amount<<"\t "<<proDis;
-                }
-                data>>pcode>>pname>>price>>dis;
-            }
-        // }
-        data.close();
-    }
-    cout<<"\n\n\t\t________________________________________________________________________________________________";
-    cout<<"\n\n\t\tTotal Amount: "<<total;
-}
-}
-int main(){
-    shopping s;
-    s.menu();
 }
